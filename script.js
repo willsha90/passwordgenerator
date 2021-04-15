7b1843c7f32899b5516337cb94e906d1bf276e95
Will <shaheenwill@gmail.com>
2021-04-15 17:56:09 -0400

    updated final string

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = ""
+var letterPool = []
 const UPPERCASE_LETTERS = ["A", "B", "C", "D"]
 const LOWERCASE_LETTERS = ["a", "b", "c", "d"]
 const NUMBERS = ["0", "1", "2", "3"]
@@ -15,6 +17,12 @@ const SPECIAL_CHARS = ["!", "@", "#", "%"]
 // functions
 function generatePassword() {
   var passwordLength = prompt("How many characters do you prefer");
+  if (passwordLength < 8 || passwordLength > 126)  { 
+    alert("password must be between 8 and 126 characters")
+    return
+  }
+
+
   console.log(passwordLength);
   var userCharacters = {}
   // - get the user's choices
@@ -33,7 +41,7 @@ function generatePassword() {
   console.log(hasUpperCase, hasLowerCase, hasNumbers, hasSpecialCharacters);
 
   // combine the chosen arrays
-  var letterPool = []
+  
   if (hasUpperCase) {
     letterPool = letterPool.concat(UPPERCASE_LETTERS)
     console.log(letterPool);
@@ -50,42 +58,43 @@ function generatePassword() {
     letterPool = letterPool.concat(SPECIAL_CHARS)
     console.log(letterPool);
   }
+  
+generateRandomPassword(passwordLength)
+
+}
   // randomly choose characters from the combined array and add them to a string called password
+  function generateRandomPassword(x) {
+    for (var i = 0; i < x; i++) {
+      var c = pickRandomItem(letterPool)
+      console.log(c);
+      password += c
+    }
+   console.log(password)
+   var passwordText = document.querySelector("#password");
+  console.log('from writepassword',password)
+  passwordText.value = password; }
+
+
   function pickRandomItem(items) {
-
     return items[Math.floor(Math.random() * items.length)];
+  }
 
-  }
-  var password = ""
-  var i;
-  for (i = 0; i < 8; i++) {
-    var c = pickRandomItem(letterPool)
-    console.log(c);
-   password.concat() 
-  }
   // return a password
 
-  return password +   
+ 
 
 
 
-  // return a "string"
+    // return a "string"
 
-}
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 
 // user interactions
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 
 // Initalization
